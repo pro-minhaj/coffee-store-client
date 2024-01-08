3073616ebd3c8a6bd6047a75dd9f34742d8d071e
Md Minhaj <parsonal322532@gmail.com>
2024-01-09 01:33:33 +0600

    Fix

--- src/Firebase/Firebase_Config.js
+++ src/Firebase/Firebase_Config.js
@@ -5,12 +5,12 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyDgiJp-1KIouF3fxK1eUiGMBIIJe4LJPoM",
-  authDomain: "coffee-store-bd.firebaseapp.com",
-  projectId: "coffee-store-bd",
-  storageBucket: "coffee-store-bd.appspot.com",
-  messagingSenderId: "312006075304",
-  appId: "1:312006075304:web:c670d96658af536ed3f39d"
+  apiKey: import.meta.env.VITE_APIKEY,
+  authDomain: import.meta.env.VITE_AUTHDOMAIN,
+  projectId: import.meta.env.VITE_PROJECTID,
+  storageBucket: import.meta.env.VITE_STORAGEBUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGINGSENDERID,
+  appId: import.meta.env.VITE_APPID,
 };
 
 // Initialize Firebase
