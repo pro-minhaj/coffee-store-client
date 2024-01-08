1334a3f09a8e9c6f7e4ddc7cf8ce4550315fe3d1
Md Minhaj <parsonal322532@gmail.com>
2024-01-08 23:26:39 +0600

    Our Popular Products Section Add Coffee Show And Full Responsive Complete

--- src/Compnents/Card/Card.jsx
+++ src/Compnents/Card/Card.jsx
@@ -5,18 +5,21 @@ import { FaPen } from "react-icons/fa";
 import { MdDelete } from "react-icons/md";
 import { Link } from 'react-router-dom';
 
-const Card = () => {
+const Card = ({card}) => {
+    const {_id, name, photo, price, chef} = card;
+
+    console.log(card);
     return (
-        <div className="bg-stone-100 rounded-[10px] flex items-center justify-between py-[30px] px-[40px] flex-wrap">
-            <div>
-                <img src={CardImg} alt="" />
+        <div className="bg-stone-100 rounded-[10px] grid grid-cols-1 md:grid-cols-6 items-center justify-between gap-5 py-[30px] px-[40px] md:space-x-10">
+            <div className='md:col-span-2'>
+                <img className='w-full h-64 md:h-44 rounded-lg' src={photo ? photo : CardImg} alt="" />
             </div>
-            <div className='flex flex-col gap-3'>
-                <h4 className="text-stone-900 text-xl font-semibold font-['Raleway']">Name: <span className="text-zinc-600 text-xl font-normal font-['Raleway']">Americano Coffee</span></h4>
-                <h4 className="text-stone-900 text-xl font-semibold font-['Raleway']">Chef: <span className="text-zinc-600 text-xl font-normal font-['Raleway']">Mr. Matin Paul</span></h4>
-                <h4 className="text-stone-900 text-xl font-semibold font-['Raleway']">Price: <span className="text-zinc-600 text-xl font-normal font-['Raleway']">890 Taka</span></h4>
+            <div className='flex flex-col gap-3 items-center md:items-start md:col-span-3'>
+                <h4 className="text-stone-900 text-xl font-semibold font-['Raleway']">Name: <span className="text-zinc-600 text-xl font-normal font-['Raleway']">{name}</span></h4>
+                <h4 className="text-stone-900 text-xl font-semibold font-['Raleway']">Chef: <span className="text-zinc-600 text-xl font-normal font-['Raleway']">{chef}</span></h4>
+                <h4 className="text-stone-900 text-xl font-semibold font-['Raleway']">Price: <span className="text-zinc-600 text-xl font-normal font-['Raleway']">{price} Taka</span></h4>
             </div>
-            <div className='flex flex-col gap-3'>
+            <div className='flex justify-center md:flex-col md:items-end gap-3 md:col-span-1'>
                 <Link ><GrView className='text-white bg-[#D2B48C] rounded-[5px] !w-10 !h-10 p-2' /></Link>
                 <FaPen className='text-white bg-[#3C393B] rounded-[5px] !w-10 !h-10 p-2' />
                 <MdDelete className='text-white bg-[#EA4744] rounded-[5px] !w-10 !h-10 p-2' />
--- src/Compnents/Popular_Products/Popular_Products.jsx
+++ src/Compnents/Popular_Products/Popular_Products.jsx
@@ -1,9 +1,17 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Link } from "react-router-dom";
 import { FaCoffee } from "react-icons/fa";
 import Card from "../Card/Card";
 
 const Popular_Products = () => {
+    const [cardData, setCardData] = useState([]);
+
+    useEffect(() => {
+        fetch('http://localhost:5000/coffee')
+        .then(res => res.json())
+        .then(data => setCardData(data))
+    }, [])
+    
     return (
         <div className='container mx-auto py-20'>
             <div className="text-center pb-10">
@@ -12,9 +20,9 @@ const Popular_Products = () => {
                 <Link to="coffee" className="bg-orange-300 mt-2 rounded-[5px] border-2 border-stone-800 flex items-center gap-3 text-white text-2xl font-normal font-['Rancho'] w-[150px] px-3 h-12 mx-auto">Add Coffee <FaCoffee className="text-[#331A15]" /></Link>
             </div>
             <div className="grid lg:grid-cols-2 gap-6">
-                <Card></Card>
-                <Card></Card>
-                <Card></Card>
+                {
+                    cardData.map(data => <Card key={data._id} card={data}></Card>)
+                }
             </div>
         </div>
     );
