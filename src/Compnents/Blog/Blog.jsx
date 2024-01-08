f60b3c2a067f9b7843a8509817822886eb5fde34
Md Minhaj <parsonal322532@gmail.com>
2024-01-09 02:14:26 +0600

    Blog Page Content Added

--- src/Compnents/Blog/Blog.jsx
+++ src/Compnents/Blog/Blog.jsx
@@ -2,8 +2,11 @@ import React from 'react';
 
 const Blog = () => {
     return (
-        <div>
-            <h1>This is blog page</h1>
+        <div className='container mx-auto py-10'>
+            <div className='shadow-2xl p-10 text-center font-medium text-2xl'>
+                <h1 className='mb-5 text-black'>This is a Blog Page</h1>
+                <h2>Coffee does more than boost your energy. A few daily cups of coffee may also lower your risk of type 2 diabetes and depression, support weight management, and help you live a longer life. Just keep in mind that experts recommend limiting caffeine if you're pregnant or nursing.</h2>
+            </div>
         </div>
     );
 };
